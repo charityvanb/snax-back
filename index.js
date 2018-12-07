4ad31d9a7cbfa37ab715ad9a254520841a3868a8
justcallmebeef <bethanymitch10@gmail.com>
2018-12-07 14:36:35 -0700

    added options route for cors

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.use('*', function (req, res) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader('Access-Control-Allow-Methods', '*');
     res.setHeader("Access-Control-Allow-Headers", "*");
-    res.end();
   });
 
 app.get('/', (req, res) => {
