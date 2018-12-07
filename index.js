9774a92a1a784c9a79308b6ec95bfc28d7f054f5
justcallmebeef <bethanymitch10@gmail.com>
2018-12-07 11:19:23 -0700

    added snack seeds and removed reviews to get snack seeds to run while we wait for users data

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const express = require('express') 
 const app = express()
+const queries = require('./queries')
 let port = process.env.PORT || 3005
 
 app.get('/', (req, res) => {
-    res.send('hello')
+    queries.listAll().then(response => res.send(response))
 })
+
+// app.get('/:id', (req, res) => {
+//     let id = req.params.id
+//     queries.getById(id).then(response => res.send(response))
+// })
+
+// app.post('/', (req, res) => {
+//     queries.createSnack(req.body).then(response => res.send(response[0]))
+// })
+
+// app.delete('/:id', (req, res) => {
+//     let id = req.params.id
+//     queries.deleteSnack(id).then(res.sendStatus(204))
+// })
+
+// app.put('/:id', (req, res) => {
+//     let id = req.params.id
+//     let body = req.body 
+//     queries.updateSnack(id, body).then(data => res.json(data[0]))
+// })
+
+// app.use((req, res, next) => {
+//     res.status(404).json({ error: { message: 'data not found' }})
+// })
+ 
+//  app.use((err, req, res, next) => {
+//     const status = err.status || 500
+//     res.status(status).json({ error: err})
+// })
 
 app.listen(port)
